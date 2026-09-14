b909ce48c1366ef55baedf1680e1832bfc4ff380
MuhammadHuzaifa881 <huzaifanasir258@gmail.com>
2026-09-14 21:23:49 +0500

    complete code updated

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -53,47 +53,48 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
     ],
     description:
       "Visualize JSON data format in form of a tree or graph. Application is made using Nextjs, Tailwindcss and Monaco Editor. Design image can be downloaded from this app.",
-    sourceCodeHref: "https://github.com/MuhammadHuzaifa881/Pakistan-health-plus.git",
-    liveWebsiteHref: "https://jsontree.vercel.app",
+    sourceCodeHref:
+      "https://github.com/MuhammadHuzaifa881/Pakistan-health-plus.git",
+    liveWebsiteHref: "https://pakistan-health-plus.vercel.app/",
   },
   {
-    name: "Kanban app",
-    favicon: "/images/projects/logos/kanban.ico",
+    name: "Mega Cart",
+    favicon: "/icons/cart.svg",
     imageUrl: [
-      "/images/projects/kanbanLight.webp",
-      "/images/projects/kanbanDark.webp",
-      "/images/projects/kanbanCardLight.webp",
+      "/images/projects/mega-cart/1.png",
+      "/images/projects/mega-cart/2.png",
+      "/images/projects/mega-cart/1.png",
     ],
     description:
       "Keep track of projects and tasks in different categories. Drag and drop card from tables. Assign user and priority to task and many more features. This app is made using Reactjs, Typescript, Tailwindcss and React-beautiful-dnd library.",
-    sourceCodeHref: "https://github.com/BUMBAIYA/kanban",
-    liveWebsiteHref: "https://kannban-board.vercel.app",
+    sourceCodeHref: "https://github.com/MuhammadHuzaifa881/Mega-cart.git",
+    liveWebsiteHref: "https://mega-cart-seven.vercel.app/",
   },
   {
-    name: "Many Games",
+    name: "React Bank",
     favicon: "/images/projects/logos/manygames.ico",
     imageUrl: [
-      "/images/projects/manyGamesDark.webp",
-      "/images/projects/manyGamesLight.webp",
-      "/images/projects/manyGames2048.webp",
-      "/images/projects/manyGamesPuzzle.webp",
-      "/images/projects/manyGamesWordle.webp",
+      "/images/projects/mega-cart/1.png",
+      "/images/projects/mega-cart/2.png",
+      "/images/projects/mega-cart/1.png",
     ],
     description:
       "Many short and fun games using Reactjs, Typescript and Tailwindcss with collaboration with other developers",
-    sourceCodeHref: "https://github.com/BUMBAIYA/ManyGames",
-    liveWebsiteHref: "https://manygames.vercel.app",
+    sourceCodeHref: "https://github.com/MuhammadHuzaifa881/Raect-Bank.git",
+    liveWebsiteHref: "https://raect-bank.vercel.app/",
   },
   {
-    name: "My portfolio",
+    name: "Blog React",
     favicon: "/favicon.ico",
     imageUrl: [
-      "/images/projects/portfolioDark.webp",
-      "/images/projects/portfolioLight.webp",
+      "/images/projects/blog-react/1.png",
+      "/images/projects/blog-react/2.png",
+      "/images/projects/blog-react/1.png",
+      "/images/projects/blog-react/2.png",
     ],
     description:
-      "My personal portfolio website made using Nextjs, tailwindcss and framer motion.",
-    sourceCodeHref: "https://github.com/BUMBAIYA/amitchauhan-v2",
+      "My personal portfolio website made using ReactJs, tailwindcss and framer motion.",
+    sourceCodeHref: "https://github.com/MuhammadHuzaifa881/cute-blog.git",
     liveWebsiteHref: siteMetadata.siteUrl,
   },
   {
@@ -106,7 +107,7 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
     ],
     description:
       "Get latest covid related stats in a table where user can filter using country and cases. It also has a map which show every countries data for quick view. It uses an open source api (disease.sh). Made using Reactjs, Javascript and MUI",
-    sourceCodeHref: "https://github.com/BUMBAIYA/CovidTracker",
+    sourceCodeHref: "https://github.com/MuhammadHuzaifa881/covid-tracker.git",
     liveWebsiteHref: "https://bumbaiya.github.io/CovidTracker",
   },
   {
@@ -120,6 +121,6 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
     ],
     description:
       "Get prediction of opening and closing price of stocks price. Frontend is made using Reactjs and backend is made using Flask and Machine learning model are trained using Tensorflow",
-    sourceCodeHref: "https://github.com/Rohrschachh/Stock-Market-Predictor",
+    sourceCodeHref: "https://github.com/MuhammadHuzaifa881/stock-predictor.git",
   },
 ];
